5eaf62dfd1f7449ca51327955da2bd4c147d74e1
Egemen Ceylan <egemen.ceylan1@gmail.com>
2022-01-04 18:34:57 +0300

    initilized firebase app in app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,10 @@
 import '../styles/main.css'
 import type { AppProps } from 'next/app'
 import { initializeApp } from 'firebase/app';
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyA70nif9XJQ_N9oQaCbFDHMd_H9L7PUHF0",
-  authDomain: "wordy-a7813.firebaseapp.com",
-  databaseURL: "https://wordy-a7813-default-rtdb.firebaseio.com",
-  projectId: "wordy-a7813",
-  storageBucket: "wordy-a7813.appspot.com",
-  messagingSenderId: "531698148251",
-  appId: "1:531698148251:web:b6f243cfb8354d69a8773e"
-};
+import { FIREBASE_CONFIG } from './shared/constants';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  initializeApp(firebaseConfig);
+  initializeApp(FIREBASE_CONFIG);
   return <Component {...pageProps} />
 }
 
---
+++ pages/shared/constants.ts
@@ -0,0 +1,11 @@
+export const FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyA70nif9XJQ_N9oQaCbFDHMd_H9L7PUHF0',
+    authDomain: 'wordy-a7813.firebaseapp.com',
+    databaseURL: 'https://wordy-a7813-default-rtdb.firebaseio.com',
+    projectId: 'wordy-a7813',
+    storageBucket: 'wordy-a7813.appspot.com',
+    messagingSenderId: '531698148251',
+    appId: '1:531698148251:web:b6f243cfb8354d69a8773e'
+};
+
+export const { databaseURL } = FIREBASE_CONFIG;
